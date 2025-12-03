bf24cdd00781f1fb22f00e4fca838149d8787424
javier garcia alvarez <javigarcial87@gmail.com>
2025-12-03 17:05:15 +0100

    separacion en otro html de las contraseñas seguras

--- JS/script.js
+++ JS/script.js
@@ -2,57 +2,7 @@ console.log("Empezamos!!")
 
 
 
-//<-------------------------------CONTRASEÑA SEGURA-------------------------------------------------------->
-// script.js
 
-function generarPass() {
-    const longitudPass = parseInt(document.getElementById("longitud").value);
-
-    if (longitudPass < 12 || longitudPass > 50) {
-        alert("La longitud debe estar entre 12 y 50");
-        return;
-    }
-
-    let Mayus   = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
-    let Minus   = "abcdefghijklmnñopqrstuvwxyz";
-    let number  = "0123456789";
-    let simbols = "!@#$%^&*()-_=+";
-
-    let pool = Mayus + Minus + number + simbols;
-
-    let password = "";
-
-    
-    password += getrandomCharacter(Mayus);
-    password += getrandomCharacter(Minus);
-    password += getrandomCharacter(number);
-    password += getrandomCharacter(simbols);
-
-    
-    for (let i = 4; i < longitudPass; i++) {
-        password += getrandomCharacter(pool);
-    }
-
-    
-    password = mezclar(password);
-
-    
-    const mostrarPassword = document.getElementById("mostrarPassword");
-    mostrarPassword.innerText = "contraseña generada :" + password; 
-}
-
-function getrandomCharacter(str) {
-    return str[Math.floor(Math.random() * str.length)];
-}
-
-function mezclar(str) {
-    let arr = str.split("");
-    for (let i = arr.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i + 1));
-        [arr[i], arr[j]] = [arr[j], arr[i]];
-    }
-    return arr.join("");
-}
 
         
 //<-----------------------------------MIS LINKS FAVORITOS------------------------------------------------------------->
@@ -69,11 +19,11 @@ const urlCook =  "https://www.allrecipes.com/"
 
 //<------------------------------------CLIMA/TEMPORAL------------------------------------------------------------->
 
-const urlClima = "https://www.weatherapi.com/"
+const urlClima = "https://api.weatherapi.com/v1"
 
+const apiKey = "7839efe96b9c4409974201359252611"
 
-
-
+const 
 
 
 
---
+++ JS/scriptpasswordSeguro.js
@@ -0,0 +1,48 @@
+function generarPass() {
+    const longitudPass = parseInt(document.getElementById("longitud").value);
+
+    if (longitudPass < 12 || longitudPass > 50) {
+        alert("La longitud debe estar entre 12 y 50");
+        return;
+    }
+
+    let Mayus   = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
+    let Minus   = "abcdefghijklmnñopqrstuvwxyz";
+    let number  = "0123456789";
+    let simbols = "!@#$%^&*()-_=+";
+
+    let pool = Mayus + Minus + number + simbols;
+
+    let password = "";
+
+    
+    password += getrandomCharacter(Mayus);
+    password += getrandomCharacter(Minus);
+    password += getrandomCharacter(number);
+    password += getrandomCharacter(simbols);
+
+    
+    for (let i = 4; i < longitudPass; i++) {
+        password += getrandomCharacter(pool);
+    }
+
+    
+    password = mezclar(password);
+
+    
+    const mostrarPassword = document.getElementById("mostrarPassword");
+    mostrarPassword.innerText = "contraseña generada :" + password; 
+}
+
+function getrandomCharacter(str) {
+    return str[Math.floor(Math.random() * str.length)];
+}
+
+function mezclar(str) {
+    let arr = str.split("");
+    for (let i = arr.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    return arr.join("");
+}
