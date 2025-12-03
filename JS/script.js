a6e44ee2ee1a821b6e33a89851ce1613fcc12915
javier garcia alvarez <javigarcial87@gmail.com>
2025-12-03 18:55:53 +0100

    urls del clima API

--- JS/script.js
+++ JS/script.js
@@ -19,8 +19,8 @@ const urlCook =  "https://www.allrecipes.com/"
 
 //<------------------------------------CLIMA/TEMPORAL------------------------------------------------------------->
 
-const urlClima = "https://api.weatherapi.com/v1/forecast.json?key=&q=Madrid&days=7&aqi=no&alerts=no"
-
+const urlClimaActual = "https://api.weatherapi.com/v1/current.json?key=7839efe96b9c4409974201359252611&q=Madrid&aqi=no"
+const  urlClimaSemanal = "https://api.weatherapi.com/v1/forecast.json?key=7839efe96b9c4409974201359252611&q=Madrid&days=7&aqi=no&alerts=no"
 const API_KEY = "7839efe96b9c4409974201359252611"
 
 const CITY = "Madrid"
