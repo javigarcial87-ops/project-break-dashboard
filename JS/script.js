14bb72050877a96f83381d25ab0b6f40cca884f2
javier garcia alvarez <javigarcial87@gmail.com>
2025-12-03 19:31:20 +0100

    cambios urls

--- JS/script.js
+++ JS/script.js
@@ -20,7 +20,7 @@ const urlCook =  "https://www.allrecipes.com/"
 //<------------------------------------CLIMA/TEMPORAL------------------------------------------------------------->
 
 const urlClimaActual = "https://api.weatherapi.com/v1/current.json?key=7839efe96b9c4409974201359252611&q=Madrid&aqi=no"
-const  urlClimaSemanal = "https://api.weatherapi.com/v1/forecast.json?key=7839efe96b9c4409974201359252611&q=Madrid&days=7&aqi=no&alerts=no"
+const urlClimaSemanal = "https://api.weatherapi.com/v1/forecast.json?key=7839efe96b9c4409974201359252611&q=Madrid&days=7&aqi=no&alerts=no"
 const API_KEY = "7839efe96b9c4409974201359252611"
 
 const CITY = "Madrid"
