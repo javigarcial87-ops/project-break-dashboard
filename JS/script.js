214b1b13cbcf28da2b702bf52f168c0bdb96b12a
javier garcia alvarez <javigarcial87@gmail.com>
2025-12-04 18:46:53 +0100

    funciones peticion fetch a la api

--- JS/script.js
+++ JS/script.js
@@ -22,14 +22,41 @@ const urlCook =  "https://www.allrecipes.com/"
 const urlClimaActual = "https://api.weatherapi.com/v1/current.json?key=7839efe96b9c4409974201359252611&q=Madrid&aqi=no"
 const urlClimaSemanal = "https://api.weatherapi.com/v1/forecast.json?key=7839efe96b9c4409974201359252611&q=Madrid&days=7&aqi=no&alerts=no"
 const API_KEY = "7839efe96b9c4409974201359252611"
-
+const baseUrl = "https://api.weatherapi.com/v1" 
 const CITY = "Madrid"
 
 const DAYS = 7
 
+const climaAhoraContainer = document.getElementById("climaAhoraContainer")
+const climaSemanaContainer = document.getElementById("climaSemanaContainer")
+const errorContainer = document.getElementById("error")
+
+async function getclimaActualMadrid (){
+    const url =`${baseUrl}/current.json?key=${API_KEY}&q=${CITY}&aqi=no`
+    const response =await fetch(url)
+
+    if (!response.ok) {
+        const text = await response.text()
+        throw new Error (`HTTP ${response.status} - ${text}`)
+    }
+
+    const data= await response.json()
+    return data
+}
+
+async function getClimaSemanal (){
+    const url =`${baseUrl}/forecast.json?key=${API_KEY}&q=${CITY}&days=${DAYS}&aqi=no&alerts=no`
+    const response =await fetch(url)
+
+    if (!response.ok) {
+        const text = await response.text()
+        throw new Error (`HTTP ${response.status} - ${text}`)
+    }
 
 
-
+    const data =await response.json()
+    return data
+}
 
 
 
